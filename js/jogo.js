6719da0ce769d94ace473daa03811b5f597678b7
Cauã Moreno <caua.moreno@estudante.ufjf.br>
2023-12-05 15:35:05 -0300

    correção de conflitos

--- js/jogo.js
+++ js/jogo.js
@@ -4,18 +4,69 @@ let dadoCtnO = document.querySelector('#dadoO');
 let tabuleiroO = document.querySelectorAll('#Oponent .Tabuleiro p');
 let tabuleiroP = document.querySelectorAll('#Player .Tabuleiro p');
 
+
 function jogarDado() {
     dado = Math.floor(Math.random() * 6) + 1;
-    this.textContent = 0;
     this.textContent = dado;
 }
 
-function posicionaDado()
+function posicionaDado(element)
 {
-    this.textContent = dado;
+    element.textContent = dado;
+}
+
+function getDadoCtnP () {
+    return dadoCtnP;
+}
+
+function getDadoCtnO () {
+    return dadoCtnO;
+}
+
+function getTabuleiroP () {
+    return tabuleiroP;
+}
+
+function getTabuleiroO () {
+    return tabuleiroO;
+}
+
+function procuraZero(tabuleiro)
+{
+    for(let i = 0; i < 9; i++)
+    {
+        if(tabuleiro[i].textContent === "0")
+        {
+            console.log(true);
+        }
+        return true;
+    }
+    console.log(false);;
+    return false;
 }
 
 let jogador = {
-    jogada: dadoCtnP.addEventListener('click', jogarDado),
-    posiciona: tabuleiroP.forEach(e => e.addEventListener("click", posicionaDado))
+    jogada: function (){
+        tabuleiroP.forEach(e => e.addEventListener('click', function(e){
+            if(e.target.textContent === "0") {
+                posicionaDado(e.target);
+                jogarDado.call(dadoCtnP);
+            }
+            oponente.jogada();
+        }));
+    }
 }
+
+let oponente = {
+    jogada: function (){
+        tabuleiroO.forEach(e => e.addEventListener('click', function(e){
+            if(e.target.textContent === "0") {
+                posicionaDado(e.target);
+                jogarDado.call(dadoCtnO);
+            }
+            jogador.jogada();
+        }));
+    }
+}
+
+export {jogarDado, posicionaDado, jogador, oponente, procuraZero, getTabuleiroP, getTabuleiroO, getDadoCtnO, getDadoCtnP}
